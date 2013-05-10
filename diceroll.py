b48fbfd7b50a4c1a653d5a1fa9fd4f482fee6c0d
pturner <pturner@mymit.local>
2013-05-09 22:27:55 -0400

    added dicefile

---
+++ diceroll.py
@@ -0,0 +1,14 @@
+#dice roller    
+
+import random
+
+def DiceRoller():
+ while True:
+    sides = raw_input('How many sided dice? (or quit) ')
+    if sides == 'quit':
+        break
+    d = int(sides)
+    roll = int(random.random()*d)+1
+    print roll
+
+DiceRoller()

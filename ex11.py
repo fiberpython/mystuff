056a33b5c0c03007095fa3b70778080c188c98cb
Mr. Paul <fiberphoton@gmail.com>
2013-10-07 16:39:38 -0400

    made changes for LPTHW

--- ex11.py
+++
@@ -1,9 +0,0 @@
-print "\nHow old are you?\n",
-age = raw_input()
-print "How tall are you?\n",
-height = raw_input()
-print "How much do you weigh?\n",
-weight = raw_input()
-
-print "So, you're %r old, %r tall and %r heavy." % (
-    age, height, weight)
--- ex36.py
+++
@@ -1,6 +0,0 @@
-
-
-
-# mymit:mystuff pturner$ perl -i -p -e '1,s/^/\#\!\/usr\/bin\/python\n/' *.py    
-
-

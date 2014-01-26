fc85b5e6d0f3d7380b4a218f9e237033dfe25577
Mr. Paul <fiberphoton@gmail.com>
2014-01-25 19:39:40 -0500

    added Say number

---
+++ say_num.py
@@ -0,0 +1,22 @@
+import random, subprocess
+
+x=int(random.uniform(1, 18))
+
+k=int(random.uniform(19, 41))
+ans = k - x
+print "\n %d - %d = %d" % (k,x,ans)
+thewrit ="""  say %d minus %d """ % (k,x)
+print thewrit
+# print "Ans = %d", ans
+
+# Format expression That is 1 dead bird!
+# call([arg])
+uname = "say"
+uname_arg = str(k)
+subprocess.call([uname, uname_arg])
+subprocess.call(["say", "minus"])
+
+uname_argx = str(x)
+subprocess.call([uname, uname_argx])
+
+# subprocess.call(["say", "%d"])

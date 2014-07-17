73009cf526a9241f0950163499119e4763684739
Fiber Photon <fiberphoton@gmail.com>
2014-07-17 09:49:26 -0400

    other random sample files

---
+++ my_heart_beat.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/* this is like a hello world program 
+  
+  and this is just multi-lined comments
+  
+   */
+
+int main()
+{
+	// again this is just a single line comment
+	
+	    printf("\nMy heat beat is around\n");
+
+	    return 0;
+
+
+
+}
+

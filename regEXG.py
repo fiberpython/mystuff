a867c8bd9db11c94513aee8e1fb58e05ec359b13
Fiber Photon <fiberphoton@gmail.com>
2014-09-23 03:46:22 -0400

    added really

--- regEXG.py
+++ regEXG.py
@@ -3,7 +3,7 @@ for test_string in ['212-536-8416', '212-1LL-EGAL']:
 
     if re.match(r'^\d{3}-\d{3}-\d{4}$', test_string):
     	
-        print test_string, 'is a valid US local phone number'
+        print test_string, 'is really a valid US local phone number'
     else:
         print test_string, 'has been rejected'
 

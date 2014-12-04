80d38d429fd838fb1ef611eff1f312f7ab12093b
Fiber Photon <fiberphoton@gmail.com>
2014-12-03 23:06:45 -0500

    added double_list a few times to see if it kept the state in between calls.

--- doublist.py
+++ doublist.py
@@ -2,8 +2,21 @@
 
 def double_list(alist):
     for i in range(len(alist)):
-        alist[i] = 3 * alist[i]
+        alist[i] = 2 * alist[i]
 
 x = [1, 2, 3, 4, 5]
 double_list(x)
 print x  
+double_list(x)
+print x
+double_list(x)
+double_list(x)
+double_list(x)
+double_list(x)
+double_list(x)
+double_list(x)
+double_list(x)
+double_list(x)
+double_list(x)
+double_list(x)
+print x

dcfebf0d77f52aa1302f3a97934b54038123f12f
Fiber Photon <fiberphoton@gmail.com>
2015-09-08 11:34:30 -0400

    cuwork.pl is much better now

--- cuwork.pl
+++ cuwork.pl
@@ -8,69 +8,25 @@ while(<FH>){
 		next if /^[ \t]*$/;		
 		next if /^#/;
 		next if /#/;
-		$line =~ s/^*\://;
-        print "$line";
+		next if (!m/@/);  # if there is no at sign then we don't need to be here NEXT
+		$line =~ s/^.*://;  #takes away the first part of the line before the :
         my @daline = split /,/, $line;
 
-	   # my ($daline)=split(",", $line);
-	    print "daline = @daline\n";
-	    print "daline 0 = @daline[0]\n";
-	    print "daline 1 = @daline[1]\n";
-	    print "daline 2 = @daline[2]\n";
 
-	    exit;
+   $n = 0;
 
-	    my (@seekihome)=split(",", @daline);
-        #print "NOTHING TO SEE HERE daline = $daline\n";
-	    #print ("GEE: @daline\n");
-	    $n = 0;
+  foreach my $val (@daline) {
 
-# USE THE SCALAR FORM OF ARRAY??
-while ($seekihome[$n]) {
-	print "inhere too $n\n";
-		$n++;
-    print ("OK I see ya > @seekihome\n");
-	print "$seekihome[$n]\tfound=$n\n";
-}
+  	next if $val !~ /\@columbia.edu/;
 
-       
-#		print ("$line");
-	    #my ($dropit,@daline)=split(":", $line);
-
-	#system (date);
+  	#    >>>  next if $val =~ (m/cornell/);
+    # print "Con[$n] $val\n";
+    # /opt/local/bin/expn $val
+    print "/opt/local/bin/expn $val\n";
+    # @ans = `/opt/local/bin/expn $val 2>&1`; # here I'm trying to capture the output from the command
+  }
 
        }
 
 
 
-
-# 		{
-# 			#if an error is found send out a page.
-# 			#print $line;
-#                         $_ = $line;
-#                         s/[^0-9a-zA-Z_\-?\w ]*//g;
-#                         $line = $_;
-# 			trap_die ( "Error  $alert_log_filename    $line" );
-# 			##print "$line\n";
-# 			last READ;
-# 		}
-# 		if ( $line !~ "^succeed" && $line =~ "[a-z]" &&(($alert_log_filename =~ "orafind") || ($alert_log_filename=~ "space_check")))
-# 		{
-# 			trap_die ( "Error  $alert_log_filename    $line" );
-# 			last READ;
-# 		}
-# 	}
-# 
-#         seek(ALERT,0,2);
-# 	my $LastPosition = tell ALERT;
-# 	close ALERT;
-# 
-#         #get new byte offset from ls command, and print into the .count.log file.
-#         open(OUTPUT, "> $alert_offset_filename");
-# 	print OUTPUT $LastPosition;
-# 	close OUTPUT;
-# $i++;
-# } # end foreach
-# system (date);
-# 
-# 

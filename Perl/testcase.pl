09b14330f88acdf29486e6cdd6ed2c567ee0d7c2
Fiber Photon <fiberphoton@gmail.com>
2015-11-12 14:33:41 -0500

    So perl test case is a look at the switch statement

---
+++ Perl/testcase.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+use Switch;
+
+@array1 = ("abc", abb, "p", "p", "nameme","abc");
+
+
+foreach $val (@array1) { 
+
+	switch($val) {
+		
+		case "abc"	{ print "you have abc\n\n" }
+		case "abb"  { print "you have port\n\n" }
+		case "p"    { print "you have funk\n\n" }
+		else		{ print "there is nothing going on here now\n\n\n" }
+
+	}
+
+}

d6fe575a34fa37cc6601a531ce7eda1ab0164de8
Fiber Photon <fiberphoton@gmail.com>
2015-12-19 15:27:14 -0500

    added adate.pl for part of solr backup

---
+++ Perl/adate.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+use POSIX qw(strftime);
+$now_string = strftime "%Y%e%m%H%M%S", localtime;
+
+print "\n\n $now_string\n\n";
+
+
+# $dDate = system("/bin/date \"+%Y%e%m%H%M%S\"");
+# print "\ndate = $dDate\n\n";
+

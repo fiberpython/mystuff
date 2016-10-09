4ec56650270f8203575a0bc954390ad7b7e3375f
Paul Turner <pat2149@columbia.edu>
2016-10-09 12:11:11 -0400

    changed the range to remove the i + thing

---
+++ pythonScripts/i_to_3_and_i_to_5.py
@@ -0,0 +1,16 @@
+
+def echo5():
+	print "This number is divisible by 5"
+def echo3():
+	print "This number is divisible by 3"
+
+
+for i in range(1, 101):
+
+	if i % 3 == 0:
+		echo3()
+		print "Fizz >> %d" % i
+	
+	if i % 5 == 0:
+		echo5()
+		print "Buzz >> %d" % i

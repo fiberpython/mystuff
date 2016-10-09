2342cf0846bd23e81e4d8a4342a6c39c3801bec6
Paul Turner <pat2149@columbia.edu>
2016-10-09 11:54:35 -0400

    I_Three_I_Five.py show you the loop with an offset of one and the Modulus Operator

---
+++ pythonScripts/I_Three_I_Five.py
@@ -0,0 +1,17 @@
+
+def echo5():
+	print "This number is divisible by 5"
+def echo3():
+	print "This number is divisible by 3"
+
+
+for i in range(100):
+	i = i + 1
+
+	if i % 3 == 0:
+		echo3()
+		print "Fizz >> %d" % i
+	
+	if i % 5 == 0:
+		echo5()
+		print "Buzz >> %d" % i

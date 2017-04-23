a55b1944bd15dd2972dced1d622f407837df6ba6
Paul Turner <paulturner41@gmail.com>
2017-04-22 21:34:47 -0400

    Created a real Generator

---
+++ pythonScripts/gen_rate_or_ex.py
@@ -0,0 +1,9 @@
+ # Build and return a list
+def firstn(n):
+    num, nums = 0, []
+    while num < n:
+        nums.append(num)
+        num += 1
+    return nums
+ 
+sum_of_first_n = sum(firstn(1000000))

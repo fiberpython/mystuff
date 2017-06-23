5692d440685b88aeab82db271fe445a6c7e38bc9
Paul Turner <paulturner41@gmail.com>
2017-06-23 08:17:28 -0400

    added some simple funcitons to pythonScripts/DrafClass.py

--- pythonScripts/DrafClass.py
+++ pythonScripts/DrafClass.py
@@ -3,6 +3,11 @@ class MyClass:
     i = 12345
     def f(self):
         return 'hello world'
+    def coda(self):
+    	return 'nothing to see here'
 
 x = MyClass()
 print x.f()
+print x.coda()
+print x.i 
+

1ed5e57acd3446d0b37f6909430f927b63a6c12a
Paul Turner <paulturner41@gmail.com>
2017-09-28 17:21:00 -0400

    made the sample1.pl file work for different size arrays

--- Perl/GetJob/Sample1.pl
+++ Perl/GetJob/Sample1.pl
@@ -1,9 +1,31 @@
 #!/usr/bin/perl
 
-@array = qw(a b c );
+# This problem take the 1st char of one string
+# and the first char of the other strings and
+# stitches it together and should handle size
+# as to exsaust the iterations
 
-print ("@array\n\n");
+@a = qw(a b c);
+@b = qw(D F H);
 
-for ($i = 0; $i < 3; ++$i) {
-  print ($array[$i], "= $i\n");
+$asize = scalar(@a);
+$bsize = scalar(@b);
+
+if ($asize == $bsize)  {
+   for ($x = 0; $x < $asize; ++$x){
+    $ans = $ans . "@a[$x]@b[$x]"
+  }
 }
+if ($asize < $bsize)  {
+
+   for ($x = 0; $x < $bsize; ++$x){
+    $ans = $ans . "@a[$x]@b[$x]"
+  }
+}
+if ($asize > $bsize)  {
+   for ($x = 0; $x < $asize; ++$x){
+    $ans = $ans . "@a[$x]@b[$x]"
+   }
+}
+
+print ("Answer is = $ans")

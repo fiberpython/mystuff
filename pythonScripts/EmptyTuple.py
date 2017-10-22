cd69487cf7ae7e8500b0a518faaf9ef271c22ab2
Paul Turner <paulturner41@gmail.com>
2017-10-22 11:52:33 -0400

    TupleType

---
+++ pythonScripts/EmptyTuple.py
@@ -0,0 +1,31 @@
+
+
+empty_tuple = ()
+test1 = ("a",)
+test2 = ("a", "b")
+test3 = ("a", "b", "c")
+
+t1 = 1,
+t2 = 1,2
+t3 = 1,2,3
+
+
+print(empty_tuple)
+print(test1)
+
+print(test2)
+
+print(test3)
+print(70*"-")
+print("Tuples without parencties")
+print(70*"-")
+
+print(t1)
+print(t2)
+print(t3)
+
+print(45*"-")
+print " "
+print "types"
+print(type(t1))
+print(type(test1))

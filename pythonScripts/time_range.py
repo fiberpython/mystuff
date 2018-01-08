583fa025793a121f4136de42eaedd1cc04d43466
Paul Turner <paulturner41@gmail.com>
2018-01-08 17:07:47 -0500

    Converted to Float

--- pythonScripts/time_range.py
+++ pythonScripts/time_range.py
@@ -4,16 +4,22 @@ import time
 #use time.time() on Linux
 
 start = time.clock()
-
+print "start=%f" % start
 for x in range(1000):
     pass
 stop = time.clock()
-
 print "range(1000)"
 
 print stop-start
+floatv = start-stop
+print "floatv = %f" % floatv
+
+print "stop=%f" % stop
+
 
 start = time.clock()
+print "start=%f" % start
+
 for x in xrange(1000):
     pass
 stop = time.clock()
@@ -21,3 +27,6 @@ stop = time.clock()
 print "xrange(1000)"
 
 print stop-start
+floatv = start-stop
+print "floatv = %f" % floatv
+print "stop=%f" % stop

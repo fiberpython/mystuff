f746060199d55b95e0315f4147e564a46c3c6b8f
Paul T via Pup1 <pault@fibershell.com>
2018-05-18 08:05:03 -0400

    im not sure of the changes

--- sub/chef/my_cookbook/metadata.rb
+++ sub/chef/my_cookbook/metadata.rb
@@ -1,7 +1,7 @@
 name             'my_cookbook'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'pault'
+maintainer_email 'fibershell@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures my_cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'

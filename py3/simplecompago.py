3b97676b30a186413b60eea805f949418c613116
Fiber Python <fiberphoton@gmail.com>
2018-12-02 18:05:40 -0500

    These are python2 prgs using argparse adn compago

---
+++ py3/simplecompago.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python
+
+import compago
+app = compago.Application()
+
+@app.command
+def greet(to="world"):
+    print "Hello,", to, "!"
+
+@app.command
+def ungreet(to="world"):
+    print "Goodbye,", to, "!"
+
+if __name__ == '__main__': app.run()
+    

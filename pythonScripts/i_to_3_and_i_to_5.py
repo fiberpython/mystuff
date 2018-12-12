978ebc5989a2907e2680f4695aaec1c1bd11738c
Fiber Python <fiberphoton@gmail.com>
2018-12-12 07:57:34 -0500

    added doc string python3

--- pythonScripts/i_to_3_and_i_to_5.py
+++ pythonScripts/i_to_3_and_i_to_5.py
@@ -1,16 +1,18 @@
+#!/Users/bssg/anaconda3/bin/python3
+
 
 def echo5():
-	print "This number is divisible by 5"
-def echo3():
-	print "This number is divisible by 3"
+    print("This number is divisible by 5")
 
+def echo3():
+    print("This number is divisible by 3")
 
 for i in range(1, 101):
 
-	if i % 3 == 0:
-		echo3()
-		print "Fizz >> %d" % i
-	
-	if i % 5 == 0:
-		echo5()
-		print "Buzz >> %d" % i
+    if i % 3 == 0:
+        echo3()
+        print("Fizz >> %d" % i)
+
+    if i % 5 == 0:
+        echo5()
+        print("Buzz >> %d" % i)

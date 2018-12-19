858766746f6794df25eb3196759ba903e7b447ba
Fiber Python <fiberphoton@gmail.com>
2018-12-19 14:04:26 -0500

    nbcact.py parsing file sketch print out.

---
+++ sub/C/nbcact.py
@@ -0,0 +1,11 @@
+#!/Users/bssg/anaconda3/bin/python
+
+data = open('sketch.txt')
+
+for each_line in data:
+    (role, line_spoken) = each_line.split(':', 1)
+    print(role, end='')
+    print(' said: ', end='')
+    print(line_spoken, end='')
+
+data.close()

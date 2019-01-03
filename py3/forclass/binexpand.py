1a8cb5a82452744f6b78ab2fb0cdc9cf938ad39e
Fiber Python <fiberphoton@gmail.com>
2019-01-03 13:04:41 -0500

    added python path to first line

--- py3/forclass/binexpand.py
+++ py3/forclass/binexpand.py
@@ -1,6 +1,7 @@
+#!/usr/local/bin/python3
 
 import itertools
-binary_numbers = [''.join(digits)
-                  for digits in itertools.product(*[['0','1'] for _ in range(10)])]
-binary_numbers[:8]
 
+binary_numbers = [''.join(digits) for digits in itertools.product(*[['0', '1'] for _ in range(10)])]
+
+binary_numbers[:8]

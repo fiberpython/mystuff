95e8e46a3afad751863182b6f09ae1dbee815bef
Fiber Python <fiberphoton@gmail.com>
2019-06-16 02:19:28 -0400

    jummping thur the sliced list

--- pythonScripts/Modlist.py
+++ pythonScripts/Modlist.py
@@ -1,6 +1,12 @@
 # I'm testing a list
+
 thislist = ["apple", "banana", "cherry","orange","bread","soft","current"]
 Sliced_List = thislist[0:4]  
 print(Sliced_List)
 sl2 = thislist[1:4]
 print(sl2) 
+# now can you loop thur this subset loop
+
+for a in sl2:
+    print(a)
+

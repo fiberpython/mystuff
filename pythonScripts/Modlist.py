3baca04fea35a5f8373547d3330972533bca6bd0
Fiber Python <fiberphoton@gmail.com>
2019-06-16 02:17:51 -0400

    saved again

--- pythonScripts/Modlist.py
+++ pythonScripts/Modlist.py
@@ -1,5 +1,6 @@
+# I'm testing a list
 thislist = ["apple", "banana", "cherry","orange","bread","soft","current"]
 Sliced_List = thislist[0:4]  
 print(Sliced_List)
 sl2 = thislist[1:4]
-print(sl2)
+print(sl2) 
